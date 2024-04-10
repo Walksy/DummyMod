ade24a59e2cdfab6fd0a089b14d2a32999965c50
Walksy <walksylol@gmail.com>
2024-04-10 09:33:46 +0100

    TEST

--- src/main/java/walksy/customkits/manager/EntityRenderManager.java
+++ src/main/java/walksy/customkits/manager/EntityRenderManager.java
@@ -16,11 +16,26 @@ public class EntityRenderManager {
     public static void handlerRender(EntityRenderDispatcher dispatcher, Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci)
     {
         ci.cancel();
-        //TODO Check clip
+        //TODO save the coords of the play when it collides with the world, and save those coords and teleport the player instantly there
         double d = MathHelper.lerp((double)tickDelta, entity.lastRenderX, entity.getX());
         double e = MathHelper.lerp((double)tickDelta, entity.lastRenderY, entity.getY());
         double f = MathHelper.lerp((double)tickDelta, entity.lastRenderZ, entity.getZ());
         float g = MathHelper.lerp(tickDelta, entity.prevYaw, entity.getYaw());
-        dispatcher.render(entity, d - cameraX, e - cameraY, f - cameraZ, g, tickDelta, matrices, vertexConsumers, dispatcher.getLight(entity, tickDelta));
+        if (EnderPearlCollisionManager.collisionMade.get(entity) == null)
+        {
+            dispatcher.render(entity, d - cameraX, e - cameraY, f - cameraZ, g, tickDelta, matrices, vertexConsumers, dispatcher.getLight(entity, tickDelta));
+        } else {
+            dispatcher.render(
+                entity,
+                EnderPearlCollisionManager.collisionMade.get(entity).getX() - cameraX,
+                EnderPearlCollisionManager.collisionMade.get(entity).getY() - cameraY,
+                EnderPearlCollisionManager.collisionMade.get(entity).getZ() - cameraZ,
+                g,
+                tickDelta,
+                matrices,
+                vertexConsumers,
+                dispatcher.getLight(entity, tickDelta)
+            );
+        }
     }
 }
